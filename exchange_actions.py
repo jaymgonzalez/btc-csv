353a04af9e73e1441cf71de45b1856bc00cc80be
Jay M Gonzalez <jaimungon@gmail.com>
2023-03-25 04:00:30 +0100

    add pybit

---
+++ exchange_actions.py
@@ -0,0 +1,30 @@
+from pybit.spot import HTTP as spot_session
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config.get('BYBIT', 'API_KEY', fallback='')
+api_secret = config.get('BYBIT', 'API_SECRET', fallback='')
+endpoint = 'https://api.bybit.com'
+
+
+
+# For ease of use, create abstraction layer to use `auth_required` functions. 
+# To use actual API, use `is_testnet` = False
+class SpotManager:
+    def __init__(self, api_key: str, api_secret: str, is_testnet: bool = True):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.endpoint = endpoint
+        if is_testnet:
+            self.endpoint = 'https://api-testnet.bybit.com'
+        self.session = spot_session(
+            endpoint=self.endpoint,
+            api_key=api_key,
+            api_secret=api_secret
+        )
+
+spot_manager = SpotManager(api_key, api_secret, is_testnet=False)
+spot_manager.session.get_wallet_balance()
