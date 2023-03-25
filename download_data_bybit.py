e0844efbd483fbad22d86c7a3239abb349098a17
Jay M Gonzalez <jaimungon@gmail.com>
2023-03-25 07:02:17 +0100

    fix bybit download

--- download_data_bybit.py
+++ download_data_bybit.py
@@ -1,6 +1,11 @@
-from pybit import inverse_perpetual
+from pybit import usdt_perpetual
 import configparser
 import os
+import datetime
+import time
+
+one_month_ago = datetime.datetime.now() - datetime.timedelta(hours=199)
+epoch_time = int(time.mktime(one_month_ago.timetuple()))
 
 
 config = configparser.ConfigParser()
@@ -10,6 +15,8 @@ api_key = config.get('BYBIT', 'API_KEY', fallback='') or os.environ.get("API_KEY
 api_secret = config.get('BYBIT', 'API_SECRET', fallback='') or os.environ.get("API_SECRET")
 endpoint = 'https://api.bybit.com'
 
-session = inverse_perpetual.HTTP(endpoint,api_key,api_secret)
+session = usdt_perpetual.HTTP(endpoint,api_key,api_secret)
 
-session.orderbook(symbol='BTCUSD')
+data = session.query_kline(symbol='BTCUSDT',interval='60',from_time=epoch_time)
+
+print(data)
