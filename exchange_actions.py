e5d0bb2603ce1a4d69aec5fe477c20239dd87b53
Jay M Gonzalez <jaimungon@gmail.com>
2023-03-25 04:19:11 +0100

    add step to pipeline

--- exchange_actions.py
+++ exchange_actions.py
@@ -1,12 +1,13 @@
 from pybit.spot import HTTP as spot_session
 import configparser
+import os
 
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-api_key = config.get('BYBIT', 'API_KEY', fallback='')
-api_secret = config.get('BYBIT', 'API_SECRET', fallback='')
+api_key = config.get('BYBIT', 'API_KEY', fallback='') or os.environ.get("API_KEY")
+api_secret = config.get('BYBIT', 'API_SECRET', fallback='') or os.environ.get("API_SECRET")
 endpoint = 'https://api.bybit.com'
 
 
