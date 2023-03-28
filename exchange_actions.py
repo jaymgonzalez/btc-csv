03c46c8c1a030db8f4cdf1a55a3cfc0b02cce683
Jay M Gonzalez <jaimungon@gmail.com>
2023-03-28 12:30:07 +0200

    add api funcs

--- exchange_actions.py
+++ exchange_actions.py
@@ -1,23 +1,7 @@
-import configparser
-import os
 import pandas as pd
+from bybit_api_functions import openPosition, closePosition, createOrder
 
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-api_key = config.get('BYBIT', 'API_KEY', fallback='') or os.environ.get("API_KEY")
-api_secret = config.get('BYBIT', 'API_SECRET', fallback='') or os.environ.get("API_SECRET")
-endpoint = 'https://api.bybit.com'
-
-test_api_key = config.get('BYBIT_TEST', 'API_KEY', fallback='') or os.environ.get("TEST_API_KEY")
-test_api_secret = config.get('BYBIT_TEST', 'API_SECRET', fallback='') or os.environ.get("TEST_API_SECRET")
-
-
-# print(test_api_key)
-# print(test_api_secret)
-
-def getPosition(csvPath='1h.csv'):
+def getCsvPosition(csvPath='1h.csv'):
     df = pd.read_csv(csvPath)
     
     num_rows, num_cols = df.shape
@@ -28,4 +12,11 @@ def getPosition(csvPath='1h.csv'):
     # Return the last value
     return 'Buy' if last_value == 1 else 'Sell'
 
-print(getPosition())
+csvPositionSide = getCsvPosition()
+openPositionSide, _ = openPosition()
+
+if openPositionSide == 0:
+    createOrder(csvPositionSide)
+elif openPositionSide != csvPositionSide:
+    closePosition()
+    createOrder(csvPositionSide)
